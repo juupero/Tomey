76087f8f1d39dfd923b969199aed2678869c7600
Jussi Ojala <jussi.ojala@midaxo.com>
2019-06-04 15:15:33 +0300

    Added classes, subclasses and methods

---
+++ TomeRater.py
@@ -0,0 +1,93 @@
+class User(object):
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.books = {}
+
+    def get_email(self):
+        return self.email
+
+    def change_email(self, address):
+        self.email = address
+        print("User {} has been updated!".format(self.name))
+
+    def __repr__(self):
+        print("User {}, email: {}, books read: {}".format(self.name, self.email, len(self.books)))
+
+    def __eq__(self, other_user):
+        if self.name == other_user.name and self.email == other_user.email:
+            return True
+        else:
+            return False
+
+    def read_book(self, book, rating = None):
+        self.books[book] = rating
+
+    def get_average_rating(self):
+        book_ratings = []
+        for rating in self.books.values():
+            if rating is not None:
+                book_ratings.append(rating)
+        return book_ratings / len(book_ratings)
+
+
+class Book(object):
+    def __init__(self, title, isbn):
+        self.title = title
+        self.isbn = isbn
+        self.ratings = []
+
+    def get_title(self):
+        return self.title
+
+    def get_isbn(self):
+        return self.isbn
+
+    def set_isbn(self, new_isbn):
+        self.isbn = new_isbn
+        print("Book {}'s ISBN has been updated!".format(self.isbn))
+
+    def add_rating(self, rating):
+        if 0 <= rating <= 4:
+            self.ratings.append(rating)
+        else:
+            print("Invalid Rating")
+
+    def __eq__(self, other_book):
+        if self.title == other_book.title and self.isbn == other_book.isbn:
+            return True
+        else:
+            return False
+
+class Fiction(Book):
+    def __init__(self, title, author, isbn):
+        super().__init__(title, isbn)
+        self.author = author
+
+    def get_author(self):
+        return self.author
+
+    def __repr__(self):
+        return "{} by {}".format(self.title, self.author)
+
+class Non_Fiction(Book):
+    def __init__(self, title, subject, level, isbn):
+        super().__init__(title, isbn)
+        self.subject = subject
+        self.level = level
+
+    def get_subject(self):
+        return self.subject
+
+    def get_level(self):
+        return self.level
+
+    def __repr__(self):
+        return "{}, a {} manual on {}".format(self.title, self.level, self.subject)
+
+
+
+
+
+
+
